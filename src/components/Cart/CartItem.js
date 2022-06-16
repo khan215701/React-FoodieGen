30af4e306ac3d906a1933dcbb9ec2c85b4a3d2f8
Khan2406 <82305740+khan215701@users.noreply.github.com>
2022-06-16 11:29:43 +0530

    add cartList module and add feature in cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,11 +1,24 @@
 import styled from "./CartItem.module.css";
 import Modal from "../UI/Modal";
+import { useContext } from "react";
+import CartContext from "../../store/CartContext";
+import CartList from "./CartList";
 
 const CartItem = (props) => {
+  const ctx = useContext(CartContext)
+  const hasCart = ctx.items.length > 0
+  const onAddCartHandler = () => {}
+  const onRemoveCartHandler = () => {}
   const cartItem = 
     <ul className={styled['cart-items']}>
-      {[{ id: "c1", name: "sushi", amount: 2, price: 25.45 }].map((item) => (
-        <li> {item.name}</li>
+      {ctx.items.map((item) => (
+       <CartList 
+            id={item.id} 
+            name={item.name} 
+            amount= {item.amount} 
+            price={item.price} 
+            onRemove={onRemoveCartHandler.bind(null, item.id)} 
+            onAdd={onAddCartHandler.bind(null, item)}/>
       ))}
     </ul>
   
@@ -14,11 +27,11 @@ const CartItem = (props) => {
     {cartItem}
     <div className={styled.total}>
       <span>Total Amount</span>
-      <span>234.45</span>
+      <span>{ctx.totalAmount}</span>
     </div>
     <div className={styled.actions}>
       <button className={styled["button--alt"]} onClick={props.onHideCart}>Close</button>
-      <button className={styled.button}>Order</button>
+      {hasCart && <button className={styled.button}>Order</button>}
     </div>
   </Modal>
   )};
